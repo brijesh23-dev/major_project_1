75f8c90fa888dd53891b686479a05add526d9d14
brijesh23-dev <rbrijesh480@gamil.com>
2025-12-30 20:03:13 +0530

    router erro

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
@@ -14,6 +13,7 @@ const userRouter = require('./routes/user.js')
 const passport = require("passport");
 const LocalStrategy = require("passport-local")
 const User = require("./models/user.js");
+const path = require("path");
 
 
 //const Mongo_Url = 'mongodb://127.0.0.1:27017/wanderlust';
@@ -27,8 +27,8 @@ async function main() {
   await mongoose.connect(dbUrl);
 };
 
-app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
+app.set("view engine","ejs");
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride('_method'));
 app.engine('ejs', ejsMate);
